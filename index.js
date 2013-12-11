9d4955a855fdae4a1305f76d8193fc27455ec8bd
Ruy Adorno <ruyadorno@hotmail.com>
2013-12-11 00:05:54 -0500

    Started showing up test string characters instead of input

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
 var readline = require('readline');
 var MuteStream = require('mute-stream');
 
@@ -8,5 +12,17 @@ var rl = readline.createInterface({
     input: process.stdin,
     output: ms
 });
+
+var file = fs.readFileSync(path.join(__dirname, 'test.js'));
+var chars = file.toString().split('');
+var count = 0;
+
+var onKeyPress = function () {
+    ms.unmute();
+    rl.output.write(chars[count++]);
+    ms.mute();
+};
+
+rl.input.on('keypress', onKeyPress);
 
 ms.mute();
