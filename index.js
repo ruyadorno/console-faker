53ff44e057330817f42ca985db838e0d282af183
Ruy Adorno <ruyadorno@hotmail.com>
2013-12-11 00:20:12 -0500

    Added evaluation of printed lines

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 var fs = require('fs');
 var path = require('path');
 var readline = require('readline');
+var _eval = require('eval');
 var MuteStream = require('mute-stream');
 
 var ms = new MuteStream();
@@ -16,11 +17,26 @@ var rl = readline.createInterface({
 var file = fs.readFileSync(path.join(__dirname, 'test.js'));
 var chars = file.toString().split('');
 var count = 0;
+var currentLine = '';
+var self = this;
+
+var print = function (str) {
+    ms.unmute();
+    rl.output.write(str);
+    ms.mute();
+};
+
+var startLine = function () {
+    print('> ');
+};
 
 var onKeyPress = function () {
-    ms.unmute();
-    rl.output.write(chars[count++]);
-    ms.mute();
+    var char = chars[count++];
+    currentLine += char;
+    if (char === '\n') {
+        _eval(currentLine, 'parse-line', self, true);
+    }
+    print(char);
 };
 
 rl.input.on('keypress', onKeyPress);
